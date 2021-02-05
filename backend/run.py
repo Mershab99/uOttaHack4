188a2e32dcb7c79bf2188f31b8cbb7499ca757fa
Mershab <mershab99@gmail.com>
2021-02-05 18:36:36 -0500

    Update REST Server

--- backend/run.py
+++ backend/run.py
@@ -49,10 +49,10 @@ if 'LOG_FOLDER' in os.environ:
 
 api = Api(app)
 
-#Resources
+# Resources
 api.add_resource(status.Status, '/status')
 
 if __name__ == "__main__":
-    app.run(debug=True, port=int(os.environ.get("PORT", 80)))
-    #DOCKER DEPLOYMENT
-    #app.run(debug=True, host='0.0.0.0', port=int(os.environ.get("PORT", 80)))
+    # app.run(debug=True, port=int(os.environ.get("PORT", 80)))
+    # DOCKER DEPLOYMENT
+    app.run(debug=True, host='0.0.0.0', port=int(os.environ.get("PORT", 80)))
