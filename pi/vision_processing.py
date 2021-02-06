7192f5c6de1e4a042f0d04b934e719b63d15a80e
Mingye Chen <61096358+mingyeeee@users.noreply.github.com>
2021-02-05 21:14:32 -0500

    Added image saving and cooldown timer

--- pi/vision_processing.py
+++ pi/vision_processing.py
@@ -17,32 +17,43 @@ raw_capture = PiRGBArray(camera, size=(640, 480))
  
 # Wait a certain number of seconds to allow the camera time to warmup
 time.sleep(0.1)
-fgbg = cv2.createBackgroundSubtractorMOG2(history = 4)
+fgbg = cv2.createBackgroundSubtractorMOG2(history = 8)
+previoustime = time.time() * 1000
 # Capture frames continuously from the camera
 for frame in camera.capture_continuous(raw_capture, format="bgr", use_video_port=True):
-
-    
+    # Performace timer
+    frametimer = time.time() *1000
     # Grab the raw NumPy array representing the image
     image = frame.array
-    # Noise processing
     blurredframe = cv2.blur(image, (3,3)) 
-    # Subtraction
     fgmask = fgbg.apply(blurredframe)
     kernel = np.ones((5,5),np.uint16)
-    # Noise processing
     fgmask = cv2.morphologyEx(fgmask, cv2.MORPH_OPEN, kernel)
     fgmask = cv2.morphologyEx(fgmask, cv2.MORPH_CLOSE, kernel)
     fgmask = cv2.morphologyEx(fgmask, cv2.MORPH_OPEN, kernel)
     cv2.imshow('frame',fgmask)
+    n_black_pixels = np.sum(fgmask == 0)
+    #print('number black pixel', n_black_pixels)
+    # check if number of white pixels exceeds threshold of 5%
+    # also check is cooldown time has passed, 500 millis current cooldown
+    if(n_black_pixels < 307200*.95 and (time.time()*1000) - previoustime >500):
+        print('movement detected!', n_black_pixels, 'time:', time.time())
+        # reset cooldown timer
+        previoustime = time.time() * 1000
+        # save image
+        filename = "{timestamp}.jpg".format(timestamp = time.time())
+        cv2.imwrite(filename, image)
     # Display the frame using OpenCV
-    cv2.imshow("Frame", image)
+    #cv2.imshow("Frame", image)
      
     # Wait for keyPress for 1 millisecond
     key = cv2.waitKey(1) & 0xFF
      
     # Clear the stream in preparation for the next frame
     raw_capture.truncate(0)
-     
+    # time it takes per frame
+    elapsedtime = (time.time()*1000) - frametimer
+    print("frame/millis", elapsedtime)
     # If the `q` key was pressed, break from the loop
     if key == ord("q"):
         break
