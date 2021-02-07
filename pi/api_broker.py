28376d981b180c5ca27eff1abf8e3273fe8f08ef
Mershab <mershab99@gmail.com>
2021-02-07 07:18:53 -0500

    Update api_broker

--- pi/api_broker.py
+++ pi/api_broker.py
@@ -1,10 +1,27 @@
 import os
+
 import requests
 
-os.environ['BACKEND_API_URL'] = ""
+os.environ['BACKEND_API_URL'] = "http://35.202.241.3:8000/"
+
+
+def status():
+    url = os.environ['BACKEND_API_URL'] + "/status/"
+    res = requests.get(url)
+    return res.json()
 
 
 def post_data(event_data, endpoint):
     url = os.environ['BACKEND_API_URL'] + endpoint
     res = requests.post(url, data=event_data)
     return res
+
+
+def get_settings():
+    url = os.environ['BACKEND_API_URL'] + "/settings-get/"
+    res = requests.get(url)
+    return res
+
+
+print(status())
+print(get_settings())
